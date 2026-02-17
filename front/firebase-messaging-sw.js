19d9df98b3ef2ffc8f56d49b98d4da558c859e5a
T.Hazekawa <tenshiro.hazekawa@ntt.com>
2026-02-17 13:03:08 +0900

    Update firebase-messaging-sw.js

--- front/firebase-messaging-sw.js
+++ front/firebase-messaging-sw.js
@@ -1,15 +1,13 @@
-// Push通知を受け取ると呼ばれる
-self.addEventListener('push', function (event) {
-    // メッセージを表示する
-    var data = {};
-    if (event.data) {
-      data = event.data.json();
-    }
-    var title = data.notification.title;
-    var message = data.notification.body;
-    event.waitUntil(
-      self.registration.showNotification(title, {
-        'body': message
-      })
-    );
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA4Ix7j42trOZRWP2HEL0Cv0X-Grr2BQ3c",
+  authDomain: "gatsby-firebase-35beb.firebaseapp.com",
+  projectId: "gatsby-firebase-35beb",
+  storageBucket: "gatsby-firebase-35beb.firebasestorage.app",
+  messagingSenderId: "432789470435",
+  appId: "1:432789470435:web:392d408c32128ef106d9d6"
 });
+
+const messaging = firebase.messaging();
